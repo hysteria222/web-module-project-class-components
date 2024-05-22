b483ad1bb64feca316a55fb6170c671a6cb1548d
HYSTERIA <129130014+hysteria222@users.noreply.github.com>
2024-05-21 23:47:23 -0500

    i forgor.

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -16,12 +16,16 @@ export default class Form extends React.Component {
   onSubmit = evt => {
     evt.preventDefault()
     this.props.addTodo(this.state.name)
+    this.setState({
+      ...this.state,
+      name: ''
+    })
   }
 
   render() {
     return (
   <form onSubmit={this.onSubmit}>
-    <input type="text" className='form-input' placeholder="Type Todo Here" onChange={this.onChange} value={this.state.name}/>
+    <input type="text" placeholder="Type Todo Here" onChange={this.onChange} value={this.state.name}/>
     <input type ="submit" />
   </form>
     )
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -2,16 +2,30 @@ import React from 'react'
 import Todo from './Todo'
 
 class TodoList extends React.Component {
+  state = {
+    showAll: true, 
+  }
+
+  toggleShowAll = evt => { 
+    this.setState({
+      ...this.state, 
+      showAll: !this.state.showAll
+    })
+  }
+
   render() {
     const toDos = this.props.toDos
     const { toggleComplete } = this.props
+    const filtered = toDos.filter(td => !td.completed || this.state.showAll)
     return (
       <div>
         <h1>Hello, {this.props.message}</h1>
       {
-        toDos.map((todo) => <Todo key={toDos.id} toDos={todo} toggleComplete={toggleComplete}/>)
-      }
+        filtered.map((todo) => <Todo key={toDos.id} toDos={todo} toggleComplete={toggleComplete}/>)
+      } 
+      <button className="todo-list-button" onClick={this.toggleShowAll}>{this.state.showAll ? 'Hide Completed' : 'Show All'}</button>
       </div>
+     
     )
   }
 }
